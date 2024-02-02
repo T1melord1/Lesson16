eb5ec9cbb86d58708d2d0e5dbf7e31176b45b279
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-02 14:24:21 +0300

    RegEX примеры

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String info = "Молоко: 256, масло: 321, печенье: 54";
+        String info = "Молоко: 256, масло: 321, печенье: 54, Шоколад: 32";
 
-        int price1 = Integer.parseInt(info.substring(info.indexOf(" "), info.indexOf(",")).trim());
-        String temp = info.substring(info.indexOf(","), info.lastIndexOf(",")).trim();
-        int price2 = Integer.parseInt(temp.substring(temp.lastIndexOf(":") + 1).trim());
-        int price3 = Integer.parseInt(info.substring(info.lastIndexOf(" ")).trim());
+        System.out.println(info.replaceAll("\\w","*"));
+        // \\s - ПРОБЕЛЫ
+        // \\d - ЦИФРЫ
+        // \\w - БУКВЫ(АНГЛЙИСКИЕ) И ЦИФРЫ
+        // \\S - ВСЁ КРОМЕ ПРОБЕЛЬНЫХ СИМВОЛОВ
+        // \\D - ВСЁ КРОМЕ ЦИФР
+        // \\W - ВСЁ КРОМЕ АНГЛИЙСИКХ БУКВ И ЦИФР
 
-        System.out.println(price1+price2+price3);
     }
 }
