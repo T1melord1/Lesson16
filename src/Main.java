acaa33f8f3c7be8f983e3c8eea9d7c2915b9a560
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-02 11:59:52 +0300

    Код находит в String info 256, 321 и 54 и складывает их в sout, если будет введено ещё четвёртое значение к примеру Шоколад то обработка будет не верной, нужны регуляры.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        String info = "Молоко: 256, масло: 321, печенье: 54";
+
+        int price1 = Integer.parseInt(info.substring(info.indexOf(" "), info.indexOf(",")).trim());
+        String temp = info.substring(info.indexOf(","), info.lastIndexOf(",")).trim();
+        int price2 = Integer.parseInt(temp.substring(temp.lastIndexOf(":") + 1).trim());
+        int price3 = Integer.parseInt(info.substring(info.lastIndexOf(" ")).trim());
+
+        System.out.println(price1+price2+price3);
     }
 }
