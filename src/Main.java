1fd453fa8fac54a2752ad2d6fe5b617314ccf935
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-02 14:28:20 +0300

    RegEX примеры

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ public class Main {
     public static void main(String[] args) {
         String info = "Молоко: 256, масло: 321, печенье: 54, Шоколад: 32";
 
-        System.out.println(info.replaceAll("\\w","*"));
+        System.out.println(info.replaceAll("\\d+","*"));
         // \\s - ПРОБЕЛЫ
         // \\d - ЦИФРЫ
         // \\w - БУКВЫ(АНГЛЙИСКИЕ) И ЦИФРЫ
         // \\S - ВСЁ КРОМЕ ПРОБЕЛЬНЫХ СИМВОЛОВ
         // \\D - ВСЁ КРОМЕ ЦИФР
         // \\W - ВСЁ КРОМЕ АНГЛИЙСИКХ БУКВ И ЦИФР
+        // [А-я]
+        // * - КВАНТОР
+        // + - КВАНТОР  \\d+ меняет все цифры заменяя значением из replacement
 
     }
 }
